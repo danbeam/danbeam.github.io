8d17ff4aa24096bc3a843c905f5386df27b76e27
Dan Beam <dbeam@chromium.org>
2017-12-03 21:42:08 -0800

    refactor

--- scrape_usac_club.py
+++ scrape_usac_club.py
@@ -1,39 +1,12 @@
-import argparse
 import bs4
-import urllib
 import re
 import sys
+import urllib
 
 
 BASE_URL = 'https://legacy.usacycling.org'
 CLUB_URL_TEMPLATE = BASE_URL + '/clubs/members.php?club=%s'
 RESULTS_URL_TEMPLATE = BASE_URL + '/results/?compid=%s'
-
-
-def get_comp_id(href):
-  maybe_id = re.search('^\/results\/?\?compid=(\d+)$', href)
-  return maybe_id.group(1) if maybe_id else None
-
-
-def make_member(a):
-  return {'comp_id': get_comp_id(a['href']), 'name': a.text.strip()}
-
-
-def make_soup(url):
-  print >> sys.stderr, 'Fetching %s...' % url
-  return bs4.BeautifulSoup(urllib.urlopen(url).read(), 'html.parser')
-
-
-def member_link(el):
-  return el.name == 'a' and el.text.strip() and el.has_attr('href') and get_comp_id(el['href'])
-
-
-def result_pair(el):
-  if el.name != 'tr':
-    return False
-  if el.has_attr('bgcolor'):
-    return True
-  return el.has_attr('class') and 'homearticlebody' in el['class']
 
 
 def date_name_permit(tr):
@@ -53,6 +26,31 @@ def field_place(tr):
   return {'field': int(field), 'place': int(place)}
 
 
+def get_comp_id(href):
+  maybe_id = re.search('^\/results\/?\?compid=(\d+)$', href)
+  return maybe_id.group(1) if maybe_id else None
+
+
+def member_link(el):
+  return el.name == 'a' and el.text.strip() and el.has_attr('href') and get_comp_id(el['href'])
+
+
+def make_member(a):
+  return {'comp_id': get_comp_id(a['href']), 'name': a.text.strip()}
+
+
+def make_soup(url):
+  return bs4.BeautifulSoup(urllib.urlopen(url).read(), 'html.parser')
+
+
+def result_pair(el):
+  if el.name != 'tr':
+    return False
+  if el.has_attr('bgcolor'):
+    return True
+  return el.has_attr('class') and 'homearticlebody' in el['class']
+
+
 def main(args):
   club_url = CLUB_URL_TEMPLATE % args.club_id
   members = map(make_member, make_soup(club_url).find_all(member_link))
@@ -60,7 +58,6 @@ def main(args):
 
   if not members:
     print >> sys.stderr, 'No members for that club found'
-    return results
 
   for member in members:
     member_url = RESULTS_URL_TEMPLATE % member['comp_id']
@@ -68,7 +65,6 @@ def main(args):
 
     if not races:
       print >> sys.stderr, 'No race results found for %s (%s)' % (member['name'], member_url)
-      continue
 
     assert len(races) % 2 == 0
     num_races = len(races) / 2
@@ -84,20 +80,28 @@ def main(args):
         result.update(field_places[i])
         results.append(result)
 
-  if args.filter_results:
-    results = filter(lambda r: r['field'] > args.min_field_size and r['place'] < args.min_place, results)
+  if args.min_field_size:
+    results = filter(lambda r: r['field'] >= args.min_field_size, results)
+
+  if args.min_place:
+    results = filter(lambda r: r['place'] >= args.min_place, results)
 
   if args.sort_by_date:
     results = sorted(results, key=lambda r: r['date'], reverse=True)
 
+  if args.max_results:
+    results = results[0:args.max_results]
+
   return results
 
+
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description="Scrape a USAC team's results")
+  import argparse
+  parser = argparse.ArgumentParser(description="Scrape a USAC club's results")
   parser.add_argument('club_id', type=int, help='USAC club ID')
-  parser.add_argument('--filter_results', type=bool, help='Whether to filter results', default=True)
-  parser.add_argument('--min_field_size', type=int, help='Minimum race field size (requires: --filter_results)', default=11)
-  parser.add_argument('--min_place', type=int, help='Minimum race place (requires: --filter_results)', default=10)
-  parser.add_argument('--sort_by_date', type=bool, help='Whether to sort results reverse chronologically', default=True)
-  results = main(parser.parse_args(sys.argv[1:]))
-  print results[0:10]
+  parser.add_argument('--max_results', type=int, help='Maxium number of results to show', default=10)
+  parser.add_argument('--min_field_size', type=int, help='Minimum race field size', default=11)
+  parser.add_argument('--min_place', type=int, help='Minimum race place', default=10)
+  parser.add_argument('--sort_by_date', type=bool, help='Sort results reverse chronologically', default=True)
+  import json
+  print json.dumps(main(parser.parse_args(sys.argv[1:])))
