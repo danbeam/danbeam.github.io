14e6dd1f512cc5e0e21edbdc71fed93f3b805f6b
Dan Beam <dan@danbeam.org>
2018-02-09 18:10:26 -0800

    fo realz

--- scrape_and_upload.sh
+++ scrape_and_upload.sh
@@ -9,6 +9,8 @@ log() {
   echo "`date`: $@" >> "$LOG_FILE"
 }
 
+log "I woke up"
+
 readonly SCRAPE_CMD="python $HERE/scrape_usac_club.py"
 readonly CLUB_ID=15733
 readonly EXTRA_ARGS="--jsonp_callback got_race_results"
@@ -16,20 +18,23 @@ readonly EXTRA_ARGS="--jsonp_callback got_race_results"
 readonly TMP_FILE="$HERE/got_race_results.js.tmp"
 readonly RESULTS_FILE="$HERE/got_race_results.js"
 
+log "scraping USAC"
 eval "$SCRAPE_CMD $CLUB_ID $EXTRA_ARGS" > "$TMP_FILE"
+log "done scraping USAC"
 
 readonly DIFF_RESULTS=$(diff "$RESULTS_FILE" "$TMP_FILE")
 
-if [ -z "$DIFF_RESULTS" ] && [ "$@" != "-f" ]; then
+if [[ -z "$DIFF_RESULTS" ]]; then
   log "same results"
   rm "$TMP_FILE"
 else
   /bin/mv "$TMP_FILE" "$RESULTS_FILE"
-  log "got new results"
+  log "new results"
   git add "$RESULTS_FILE"
-  git commit -m 'new results'
-  eval `ssh-agent`
-  ssh-add race_results.rsa
-  git push origin master
-  eval `ssh-agent -k`
+  git commit -q -m 'new results'
+  { eval `ssh-agent`; ssh-add race_results.rsa; } &>/dev/null
+  git push -q origin master
+  { eval `ssh-agent -k`; } &>/dev/null
 fi
+
+log "going back to bed"
